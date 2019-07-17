76117eedc7e8208a900492e465cffedae518a937
Antonio Miguel Marín Caro - Ceiba Software <antonio.marin@ceiba.com.co>
2019-07-17 15:22:05 -0500

    inicio

---
+++ src/main/java/co/com/ceiba/ParqueaderoCeibaApplication.java
@@ -0,0 +1,13 @@
+package co.com.ceiba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParqueaderoCeibaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParqueaderoCeibaApplication.class, args);
+	}
+
+}
