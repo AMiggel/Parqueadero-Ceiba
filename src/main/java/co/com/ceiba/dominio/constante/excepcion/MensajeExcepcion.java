c329ec286a6630e107268859a3d468f444fe45ae
AMiggel <antonnio762@gmail.com>
2019-07-19 08:39:39 -0500

    Constantes y Excepciones

---
+++ src/main/java/co/com/ceiba/dominio/constante/excepcion/MensajeExcepcion.java
@@ -0,0 +1,16 @@
+package co.com.ceiba.dominio.constante.excepcion;
+
+public class MensajeExcepcion {
+	
+	private MensajeExcepcion() {
+		throw new IllegalStateException("MensajeException class");
+	}
+	
+	public static final String PLACA_INICIA_CON_A= "No esta autorizado para ingresar";
+	public static final String NO_PUEDE_PARQUEAR_MAS_CARROS="Parqueadero para carros lleno";
+	public static final String NO_PUEDE_PARQUEAR_MAS_MOTOS="Parqueadero para motos lleno";
+	public static final String VEHICULO_NO_ENCONTRADO_EN_PARQUEADERO= "El vehiculo no se encuentra parqueado";
+	public static final String VEHICULO_PARQUEADO="El vehiculo ya se encuentra parquado";
+
+
+}
