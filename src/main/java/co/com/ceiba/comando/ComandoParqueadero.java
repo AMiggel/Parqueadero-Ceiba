84d373f1233dcf000f5ca126c1e1331b81ec6a0c
AMiggel <antonnio762@gmail.com>
2019-07-19 10:20:50 -0500

    Sin lombok

--- src/main/java/co/com/ceiba/comando/ComandoParqueadero.java
+++ src/main/java/co/com/ceiba/comando/ComandoParqueadero.java
@@ -2,9 +2,7 @@ package co.com.ceiba.comando;
 
 import co.com.ceiba.dominio.modelo.entidad.Vehiculo;
 
-import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
 public class ComandoParqueadero {
 	
 	private Vehiculo vehiculo;
