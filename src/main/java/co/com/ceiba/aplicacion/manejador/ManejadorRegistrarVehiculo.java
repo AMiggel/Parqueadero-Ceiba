5030cb6f5cd641da7c71fa3149d32e7322d103e7
AMiggel <antonnio762@gmail.com>
2019-07-25 14:40:38 -0500

    conflictos arreglados

--- src/main/java/co/com/ceiba/aplicacion/manejador/ManejadorRegistrarVehiculo.java
+++ src/main/java/co/com/ceiba/aplicacion/manejador/ManejadorRegistrarVehiculo.java
@@ -24,8 +24,6 @@ public class ManejadorRegistrarVehiculo {
 		return servicioRegistrarVehiculo.buscarVehiculoParqueado(placa);
 	}
 
-	public void mostrarCuposDisponibles(String tipoVehiculo) {
-		servicioRegistrarVehiculo.verificarCupoDisponible(tipoVehiculo);
-	}
+
 
 }
